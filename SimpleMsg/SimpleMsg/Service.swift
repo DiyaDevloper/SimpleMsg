b30d8d933d381532bbd01a7065f8dbe1ae62fee7
Kartik Jasolia <kartikjasolia@Kartiks-iMac.local>
2019-01-04 18:11:50 +0530

    Finished SimleMsg

---
+++ SimpleMsg/SimpleMsg/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  SimpleMsg
+//
+//  Created by Kartik Jasolia on 04/01/19.
+//  Copyright © 2019 Kartik Jasolia. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func doSomthing() -> String {
+        return "Hello. How are you."
+    }
+}
---
+++ SimpleMsg/SimpleMsg/SimpleMsg.h
@@ -0,0 +1,19 @@
+//
+//  SimpleMsg.h
+//  SimpleMsg
+//
+//  Created by Kartik Jasolia on 04/01/19.
+//  Copyright © 2019 Kartik Jasolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleMsg.
+FOUNDATION_EXPORT double SimpleMsgVersionNumber;
+
+//! Project version string for SimpleMsg.
+FOUNDATION_EXPORT const unsigned char SimpleMsgVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleMsg/PublicHeader.h>
+
+
